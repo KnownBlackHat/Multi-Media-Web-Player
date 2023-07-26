e067269d1af8608121bbdfff80472d38b7f9eed1
Known_Black_Hat <knownblackhat@gmail.com>
2023-07-26 17:02:04 -0400

    Initial Commit

---
+++ src/components/MediaCard.svelte
@@ -0,0 +1,25 @@
+<script>
+export let src;
+let downloadBtn;
+</script>
+
+<div class="rounded border-2 border-white overflow-hidden"
+on:mouseenter={() => {downloadBtn.style.visibility="visible"}}
+on:mouseleave={() => {downloadBtn.style.visibility="hidden"}}
+>
+    <div class="relative invisible" bind:this={downloadBtn}>
+    <span class="absolute top-0 right-0 p-1">
+        <a class="bg-black text-white p-1 rounded" href={src} target="_blank" download>
+            🔗
+        </a>
+    </span>
+    </div>
+{#if src.match(/\.(jpe?g|png|gif)/)}
+    <img class="h-48 w-full rounded" src={src} alt={src} />
+{:else}
+    <video class="h-48 w-full rounded" src={src} controls loop
+    on:mouseenter={e => e.target.play()}>
+        <track kind="captions" />
+    </video>
+{/if}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+import MediaCard from "../components/MediaCard.svelte"
+let input="";
+let links=null;
+$: input? links=input.split(" "): null
+$: console.log(links)
+</script>
+<style>
+    :global(body){
+        background-color: black;
+        color: white;
+        }
+</style>
+    <header class="p-4">
+        <div class="text-4xl m-2 mb-6 text-center underline">Multi Media Web Player</div>
+        <input type="text" bind:value={input} class="rounded bg-gray-500 p-2 w-full text-center" placeholder="Paste your links here separated by space"/>
+    </header>
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {#if links}
+                {#each links as link}
+                    <MediaCard src={link} />
+                {/each}
+            {/if}
+        </div>
