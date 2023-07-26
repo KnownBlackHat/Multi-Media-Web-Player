3ff897e8cb8dcb391b2416e4370e16e040a086cd
Known_Black_Hat <knownblackhat@gmail.com>
2023-07-26 18:50:12 -0400

    perf: add pagenation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
 import MediaCard from "../components/MediaCard.svelte"
 let input="";
 let links=null;
+let toLoad=[];
 $: input? links=input.split(" "): null
-$: console.log(links)
+$: input? toLoad = [...links.splice(0, 16)] : null
 </script>
 <style>
     :global(body){
@@ -11,14 +12,26 @@ $: console.log(links)
         color: white;
         }
 </style>
-    <header class="p-4">
-        <div class="text-4xl m-2 mb-6 text-center underline">Multi Media Web Player</div>
-        <input type="text" bind:value={input} class="rounded bg-gray-500 p-2 w-full text-center" placeholder="Paste your links here separated by space"/>
-    </header>
-        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
-            {#if links}
-                {#each links as link, index (index)}
-                    <MediaCard src={link} />
-                {/each}
-            {/if}
+
+<svelte:head>
+    <title>Multi Media Web Player</title> 
+</svelte:head>
+
+<header class="p-4">
+    <div class="text-4xl m-2 mb-6 text-center underline">Multi Media Web Player</div>
+    <input type="text" bind:value={input} class="rounded bg-gray-500 p-2 w-full text-center" placeholder="Paste your links here separated by space"/>
+</header>
+
+    {#if links}
+        <div class="mx-2 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {#each toLoad as link}
+                <MediaCard src={link} />
+            {/each}
         </div>
+        {#if links.length > 0}
+                <button class="p-2 my-4 text-center border-2 border-green-500 rounded w-full"
+                on:click={()=>{
+                    toLoad = [...toLoad, ...links.splice(0, toLoad.length + 8)]
+                    }}>Load More</button>
+        {/if}
+    {/if}
